e1dcd219c833384d479e883e34e8d4ca638d5a51
ethan roman <31373652+cfox4ever@users.noreply.github.com>
2022-04-14 21:15:29 -0400

    addStop

---
+++ src/components/Booking/Address.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="row">
+    <div class="col-6">
+      <q-input
+        clearable
+         dense
+        rounded 
+        outlined
+        class="col q-ma-xs"
+        label="Address"
+        
+        bg-color="amber-12"
+        v-model="address1"
+        subtitle="Address"
+         :rules="[val => !!val || 'Field is required']"
+      />
+    </div>
+    <div class="col-6">
+      <q-input
+         dense
+        rounded 
+        outlined
+        clearable
+        class="col q-ma-xs"
+        label="Address APT, Suite, Floor, etc."
+        bg-color="amber-12"
+        v-model="address2"
+        subtitle="Address APT, Suite, Floor, etc."
+      />
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-4">
+      <q-input
+        clearable
+         dense
+        rounded 
+        outlined
+        class="col q-ma-xs"
+        label="City"
+        bg-color="amber-12"
+        v-model="city"
+        subtitle="City"
+         :rules="[val => !!val || 'Field is required']"
+      />
+    </div>
+    <div class="col-4">
+      <q-select
+         dense
+         :options="states"
+        rounded 
+        outlined
+        clearable
+        class="col q-ma-xs"
+        label="State"
+        bg-color="amber-12"
+        v-model="state"
+        subtitle="State"
+         :rules="[val => !!val || 'Field is required']"
+      />
+    </div>
+    <div class="col-4">
+      <q-input
+         dense
+         type="number"
+        rounded 
+        outlined
+        clearable
+        class="col q-ma-xs"
+        label="Zip"
+        bg-color="amber-12"
+        v-model="zip_code"
+        subtitle="Zip Code"
+         :rules="[val => !!val || 'Field is required']"
+      />
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref, onMounted, reactive } from "vue";
+const address1 = ref("");
+const address2 = ref("");
+const city = ref("");
+const state = ref("");
+const zip_code = ref("");
+const states =['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming']
+
+</script>
--- src/components/Booking/RideInfo.vue
+++ src/components/Booking/RideInfo.vue
@@ -1,37 +1,74 @@
 <template>
-  <div class="q-pa-md row items-start q-gutter-md">
+  <div >
     <q-card dark bordered class="bg-dark my-card">
       <q-card-section>
         <div class="row">
-          <q-input clearable required filled class="col q-ma-xs" label="First Name" v-model="fname" type="text"
-            bg-color="amber-12">
+          <q-input
+            clearable
+            required
+            filled
+            class="col q-ma-xs"
+            label="First Name"
+            v-model="fname"
+            type="text"
+            bg-color="amber-12"
+          >
             <template v-slot:prepend>
               <q-icon name="person" />
             </template>
           </q-input>
-          <q-input clearable filled class="col q-ma-xs" label="Last Name" v-model="lname" type="text"
-            bg-color="amber-12">
+          <q-input
+            clearable
+            filled
+            class="col q-ma-xs"
+            label="Last Name"
+            v-model="lname"
+            type="text"
+            bg-color="amber-12"
+          >
             <template v-slot:prepend>
               <q-icon name="person3" />
             </template>
           </q-input>
         </div>
         <div class="row">
-          <q-input clearable class="col q-ma-xs" label="Email" v-model="email" type="email" bg-color="amber-12">
+          <q-input
+            clearable
+            class="col q-ma-xs"
+            label="Email"
+            v-model="email"
+            type="email"
+            bg-color="amber-12"
+          >
             <template v-slot:prepend>
               <q-icon name="mail" />
             </template>
           </q-input>
-          <q-input clearable class="col q-ma-xs" label="Phone" bg-color="amber-12" v-model="phone"
-            mask="(###) ### - ####">
+          <q-input
+            clearable
+            class="col q-ma-xs"
+            label="Phone"
+            bg-color="amber-12"
+            v-model="phone"
+            mask="(###) ### - ####"
+          >
             <template v-slot:prepend>
               <q-icon name="phone" />
             </template>
           </q-input>
         </div>
         <div class="row">
-          <q-select clearable filled bg-color="amber-12" class="col q-ma-xs" v-model="service" :options="serviceOptions"
-            label="Service">
+          <q-select
+            clearable
+            filled
+            bg-color="amber-12"
+            class="col q-ma-xs"
+            v-model="service"
+            :options="serviceOptions"
+            label="Service"
+                    transition-show="scale"
+        transition-hide="scale"
+          >
             <template v-slot:prepend>
               <q-icon name="directions_car_filled" />
             </template>
@@ -39,16 +76,44 @@
         </div>
         <div class="row">
           <div class="col">
-            <q-input clearable class="col q-ma-xs" label="Pickup Date" bg-color="amber-12" v-model="fdate"
-              subtitle="Pick UP Date">
+            <q-input
+              clearable
+              class="col q-ma-xs"
+              label="Pickup Date"
+              bg-color="amber-12"
+              v-model="fdate"
+              subtitle="Pick UP Date"
+            >
               <template v-slot:prepend>
                 <q-btn icon="event" round>
-                  <q-popup-proxy @before-show="updateProxy" cover transition-show="scale" transition-hide="scale">
-                    <q-date v-model="fdate" :options="dateOptions" title="Pick UP Date" mask="MM-DD-YYYY" color="dark"
-                      text-color="amber-12">
+                  <q-popup-proxy
+                    @before-show="updateProxy"
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
+                    <q-date
+                      v-model="fdate"
+                      :options="dateOptions"
+                      title="Pick UP Date"
+                      mask="MM-DD-YYYY"
+                      color="dark"
+                      text-color="amber-12"
+                    >
                       <div class="row items-center justify-end q-gutter-sm">
-                        <q-btn label="Cancel" color="primary" flat v-close-popup />
-                        <q-btn label="OK" color="primary" flat @click="save" v-close-popup />
+                        <q-btn
+                          label="Cancel"
+                          color="primary"
+                          flat
+                          v-close-popup
+                        />
+                        <q-btn
+                          label="OK"
+                          color="primary"
+                          flat
+                          @click="save"
+                          v-close-popup
+                        />
                       </div>
                     </q-date>
                   </q-popup-proxy>
@@ -57,16 +122,43 @@
             </q-input>
           </div>
           <div class="col">
-            <q-input clearable class="col q-ma-xs" label="Pickup Time" bg-color="amber-12" v-model="time"
-              mask="##:## AM">
+            <q-input
+              clearable
+              class="col q-ma-xs"
+              label="Pickup Time"
+              bg-color="amber-12"
+              v-model="time"
+              mask="##:## AM"
+            >
               <template v-slot:prepend>
                 <q-btn icon="access_time" round>
-                  <q-popup-proxy @before-show="updateProxy" cover transition-show="scale" transition-hide="scale">
+                  <q-popup-proxy
+                    @before-show="updateProxy"
+                    cover
+                    transition-show="scale"
+                    transition-hide="scale"
+                  >
                     <div class="bg-dark">
-                      <q-time color="dark" text-color="amber-12" v-model="time" mask="hh:mm A">
+                      <q-time
+                        color="dark"
+                        text-color="amber-12"
+                        v-model="time"
+                        mask="hh:mm A"
+                      >
                         <div class="row items-center justify-end q-gutter-sm">
-                          <q-btn label="Cancel" color="primary" flat v-close-popup />
-                          <q-btn label="OK" color="primary" flat @click="save" v-close-popup />
+                          <q-btn
+                            label="Cancel"
+                            color="primary"
+                            flat
+                            v-close-popup
+                          />
+                          <q-btn
+                            label="OK"
+                            color="primary"
+                            flat
+                            @click="save"
+                            v-close-popup
+                          />
                         </div>
                       </q-time>
                     </div>
@@ -76,58 +168,12 @@
             </q-input>
           </div>
         </div>
-        <div class="row">
-          <label
-            class="q-field row no-wrap items-start q-field--filled q-input q-field--float q-field--labeled col q-ma-xs"
-            for="Gpickup">
-            <!---->
-            <div class="q-field__inner relative-position col self-stretch">
-              <div class="q-field__control relative-position row no-wrap bg-amber-12" tabindex="-1">
-                <div class="q-field__prepend q-field__marginal row no-wrap items-center">
-                  <i class="q-icon notranslate material-icons" aria-hidden="true" role="presentation">location_on</i>
-                </div>
-                <div class="q-field__control-container col relative-position row no-wrap q-anchor--skip">
-                  <input class="q-field__native q-placeholder" tabindex="0" aria-label="Pickup Address" id="Gpickup"
-                    type="text" v-model="pickup" />
-                  <div class="q-field__label no-pointer-events absolute ellipsis">
-                    Pickup Address
-                  </div>
-                  <!---->
-                </div>
-                <div class="q-field__append q-field__marginal row no-wrap items-center q-anchor--skip">
-                  <button class="q-icon notranslate material-icons q-field__focusable-action" tabindex="0" type="button"
-                    @click="pickup = ''">
-                    cancel
-                  </button>
-                </div>
-              </div>
-              <!---->
-            </div>
-            <!---->
-          </label>
-        </div>
-        <!-- <div class="row">
-          <q-input
-            clearable
-            filled
-            class="col q-ma-xs"
-            label="Pickup Address"
-            v-model="pickup"
-            type="text"
-            bg-color="amber-12"
-                       
-          :id="Gpickup"
-           
-            
-             
-          >
-            <template v-slot:prepend>
-              <q-icon name="location_on"  />
-            </template>
-          </q-input>
-         
-        </div> -->
 
+      <q-separator color="white" inset />
+<Address />
+        
+        
+ <q-separator color="green" inset />
         <div class="row pn-right" v-for="(stop, index) in stops">
           <q-btn rounded color="amber-12" class="text-black q-ma-xs">
             Stop # {{ index + 1 }} --> {{ stop }}
@@ -135,17 +181,29 @@
           </q-btn>
         </div>
 
-        <div class="row">
-          <AddStop v-model="stop" v-if="addStopField"  />
-        </div>
+        
+          <Address v-model="stop" v-if="addStopField" />
+   
 
         <div class="frow on-left">
-          <q-btn label=" + Add Stop" color="amber-12" flat @click="addStopField = true" />
+          <q-btn
+            label=" + Add Stop"
+            color="amber-12"
+            flat
+            @click="addStopField = true"
+          />
         </div>
 
         <div class="row">
-          <q-input clearable filled class="col q-ma-xs" label="Drop-off Address" v-model="dropoff" type="text"
-            bg-color="amber-12">
+          <q-input
+            clearable
+            filled
+            class="col q-ma-xs"
+            label="Drop-off Address"
+            v-model="dropoff"
+            type="text"
+            bg-color="amber-12"
+          >
             <template v-slot:prepend>
               <q-icon name="location_off" />
             </template>
@@ -154,16 +212,30 @@
 
         <div class="row">
           <div class="col">
-            <q-input clearable filled class="col q-ma-xs" label="Number of Passengers" v-model="passengers"
-              type="number" bg-color="amber-12">
+            <q-input
+              clearable
+              filled
+              class="col q-ma-xs"
+              label="Number of Passengers"
+              v-model="passengers"
+              type="number"
+              bg-color="amber-12"
+            >
               <template v-slot:prepend>
                 <q-icon name="people" />
               </template>
             </q-input>
           </div>
           <div class="col">
-            <q-input clearable filled class="col q-ma-xs" label="Luggage Count" v-model="luggage" type="number"
-              bg-color="amber-12">
+            <q-input
+              clearable
+              filled
+              class="col q-ma-xs"
+              label="Luggage Count"
+              v-model="luggage"
+              type="number"
+              bg-color="amber-12"
+            >
               <template v-slot:prepend>
                 <q-icon name="luggage" />
               </template>
@@ -172,30 +244,55 @@
         </div>
 
         <div class="row">
-          <q-input clearable filled class="col q-ma-xs" label="Child Seat" v-model="child" type="number"
-            bg-color="amber-12">
+          <q-input
+            clearable
+            filled
+            class="col q-ma-xs"
+            label="Child Seat"
+            v-model="child"
+            type="number"
+            bg-color="amber-12"
+          >
             <template v-slot:prepend>
               <q-icon name="child_care" />
             </template>
           </q-input>
         </div>
         <div class="row">
-          <q-input clearable filled class="col q-ma-xs" label="Special Instructions" v-model="instructions"
-            type="textarea" bg-color="amber-12">
+          <q-input
+            clearable
+            filled
+            class="col q-ma-xs"
+            label="Special Instructions"
+            v-model="instructions"
+            type="textarea"
+            bg-color="amber-12"
+          >
             <template v-slot:prepend>
               <q-icon name="comment" />
             </template>
           </q-input>
         </div>
         <div class="row">
-          <q-icon name="accessible" size="lg" :color="accessible ? 'green' : 'white'" />
+          <q-icon
+            name="accessible"
+            size="lg"
+            :color="accessible ? 'green' : 'white'"
+          />
 
           <q-icon name="help" :color="accessible ? 'green' : 'white'">
-            <q-tooltip>We collect this data to ensure our vehicles/staff meet your
-              individual needs.</q-tooltip>
+            <q-tooltip
+              >We collect this data to ensure our vehicles/staff meet your
+              individual needs.</q-tooltip
+            >
           </q-icon>
-          <q-toggle class="col q-ma-xs" :label="accessible ? 'Accessible Needed' : 'No Accessible'" v-model="accessible"
-            color="green" keep-color />
+          <q-toggle
+            class="col q-ma-xs"
+            :label="accessible ? 'Accessible Needed' : 'No Accessible'"
+            v-model="accessible"
+            color="green"
+            keep-color
+          />
         </div>
       </q-card-section>
     </q-card>
@@ -203,34 +300,34 @@
 </template>
 <script setup>
 import { ref, onMounted, reactive } from "vue";
-import AddStop from './AddStop.vue'
+import AddStop from "./AddStop.vue";
 
-import { date } from 'quasar'
-const timeStamp = Date.now()
+import { date } from "quasar";
+import Address from "./Address.vue";
 
+const timeStamp = Date.now();
 
-const addStopField = ref(false)
-const stops = ref([])
-const stop = ref('')
+const addStopField = ref(false);
+const stops = ref([]);
+const stop = ref("");
 
-const fname = ref("")
-const lname = ref("")
-const email = ref("")
-const phone = ref("")
-const service = ref("")
-const fdate = ref('')
-const time = ref("")
-const pickup = ref("")
-const dropoff = ref("")
-const passengers = ref("")
-const luggage = ref("")
-const child = ref("")
-const instructions = ref("")
-const accessible = ref(false)
+const fname = ref("");
+const lname = ref("");
+const email = ref("");
+const phone = ref("");
+const service = ref("");
+const fdate = ref("");
+const time = ref("");
+const pickup = ref("");
+const dropoff = ref("");
+const passengers = ref("");
+const luggage = ref("");
+const child = ref("");
+const instructions = ref("");
+const accessible = ref(false);
 
 // const formattedDate = date.formatDate(timeStamp, 'MM-DD-YYYY')
 // fdate.value = formattedDate
-
 
 const serviceOptions = ref([
   "Airport Arrival",
@@ -240,28 +337,23 @@ const serviceOptions = ref([
   "Other",
 ]);
 
-
 const addToStops = () => {
-  stops.value.push(stop.value)
-  stop.value = ''
-  addStopField.value = false
-}
+  stops.value.push(stop.value);
+  stop.value = "";
+  addStopField.value = false;
+};
 const deleteStop = (index) => {
-  stops.value.splice(index, 1)
-}
+  stops.value.splice(index, 1);
+};
 const dateOptions = (fdate) => {
   var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
+  var dd = String(today.getDate()).padStart(2, "0");
+  var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+  var yyyy = today.getFullYear();
 
-today = yyyy + '/' + mm + '/' + dd;
-  return fdate >= today
+  today = yyyy + "/" + mm + "/" + dd;
+  return fdate >= today;
 };
-onMounted( () => {
-  new google.maps.places.Autocomplete(document.getElementById("Gpickup"));
-})
-
 </script>
 
 <!-- <script>
